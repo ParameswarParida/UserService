166de59a71ad645cb8727db70711ac27ef336cce
ParameswarParida <parameswar.parida@gmail.com>
2026-09-03 20:18:12 +0530

    First Commit

---
+++ src/main/java/com/myproject/users/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.myproject.users.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String test() {
+        return "Heloo From UserController";
+    }
+
+}
